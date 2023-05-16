aa3c2be48101f867e44eda343337319d46e00dda
brunocoletti <bruno.coletti@compliancesolucoes.com.br>
2023-05-16 17:17:37 -0300

    criacao do gitignore

--- conexao-db.py
+++ conexao-db.py
@@ -1,9 +1,8 @@
 import cx_Oracle # python3 -m pip install cx_Oracle --upgrade
+import os
+from dotenv import load_dotenv
 
-#PARAM_DRIVER_ORCL  =oracle.jdbc.OracleDriver
-#PARAM_URL_ORCL    =jdbc:oracle:thin:@dbqa.cndgxb5man6u.us-east-2.rds.amazonaws.com:1521:qa
-#PARAM_USER_ORCL    =csf_own
-#PARAM_PASS_ORCL    =AdM#QA2020
+load_dotenv()
 
 ORCLPDB1 ='''(DESCRIPTION =
     (ADDRESS = (PROTOCOL = TCP)(HOST = dbqa.cndgxb5man6u.us-east-2.rds.amazonaws.com)(PORT = 1521))
@@ -15,10 +14,11 @@ ORCLPDB1 ='''(DESCRIPTION =
 #-----
 
 def conect(empresa):
-    connection = cx_Oracle.connect(user="csf_own", password="AdM#QA2020", dsn=ORCLPDB1,encoding="UTF-8")
+    connection = cx_Oracle.connect(user=os.environ.get('USR'), password=os.environ.get('PASSWD'), dsn=ORCLPDB1,encoding="UTF-8")
 
     cursor = connection.cursor()
     for result in cursor.execute("select sysdate from dual"):
         print(result)
 
     connection.close()
+conect('a')
